132ba5e5a307d53eeaf8cdf327c1745e82f22093
Rash499 <rashmikadilmin499@gmail.com>
2026-09-08 23:09:49 +0530

    added more parameters to the countries details

--- earth-globe/src/components/globe/types.ts
+++ earth-globe/src/components/globe/types.ts
@@ -1,19 +1,33 @@
-import type { Feature, FeatureCollection, Geometry } from "geojson";
+import type { Feature, Geometry } from "geojson";
 
-export type CountryProperties = { name?: string; [key: string]: unknown };
+export type CountryProperties = {
+  name?: string;
+  [key: string]: unknown;
+};
 
-export type CountryFeature = Feature<Geometry, CountryProperties> & {
+export type CountryFeature = Feature<
+  Geometry,
+  CountryProperties
+> & {
   id?: string | number;
 };
 
 export type CountryInfo = {
   name: string;
+  officialName?: string;
   capital?: string;
   population?: number;
+  area?: number;
   region?: string;
+  subregion?: string;
   flag?: string;
+  currencies?: string[];
+  languages?: string[];
+  continents?: string[];
 };
 
 export type TopoJSON = {
-  objects?: { countries?: unknown };
+  objects?: {
+    countries?: unknown;
+  };
 };
